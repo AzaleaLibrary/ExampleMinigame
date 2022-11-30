faa4d5fa0beb190d4d9cde8d533e1e552d49db46
Noah <56337775+iamthenoah@users.noreply.github.com>
2022-11-30 21:18:29 +0100

    Update ExampleMinigame.java

--- src/main/java/com/azalealibrary/example/ExampleMinigame.java
+++ src/main/java/com/azalealibrary/example/ExampleMinigame.java
@@ -10,30 +10,21 @@ import org.bukkit.plugin.java.annotation.plugin.Plugin;
 import java.io.File;
 
 @SuppressWarnings("unused")
-@Plugin(name = "ExampleMinigame", version = "1.0")
-@ApiVersion(ApiVersion.Target.v1_13) // compatible with all post-1.13 mc versions
 @Dependency("AzaleaCore")
 @LoadBefore("AzaleaCore")
+@Plugin(name = "ExampleMinigame", version = "1.0")
+@ApiVersion(ApiVersion.Target.v1_13) // compatible with all post-1.13 mc versions
 public final class ExampleMinigame extends JavaPlugin {
 
-    public static ExampleMinigame INSTANCE;
-
-    public ExampleMinigame() {}
+    public ExampleMinigame() { }
 
     public ExampleMinigame(JavaPluginLoader loader, PluginDescriptionFile description, File dataFolder, File file) {
         super(loader, description, dataFolder, file);
     }
 
     @Override
-    public void onLoad() {
-        INSTANCE = this;
-    }
-
-    @Override
     public void onEnable() {
+        // register our minigame registry
         AzaleaRegistry.EVENT_BUS.register(new ExampleRegistry());
     }
-
-    @Override
-    public void onDisable() {  }
 }
